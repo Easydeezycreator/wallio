8a893141155c9bcafafafbd61ddb400a446ea5b3
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 15:14:10 -0700

    parte v12: sin descripciones, logo grande, pagina llena

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v11";
+const VERSION = "wallio-parte-v12";
 const BASICOS = [
   "./",
   "./index.html",
