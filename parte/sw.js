513071c14f3f04a32d9c221c87ec383a01c6cc9b
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 14:29:44 -0700

    parte v10: portada con vuelta atras, buzon conectado

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v9";
+const VERSION = "wallio-parte-v10";
 const BASICOS = [
   "./",
   "./index.html",
