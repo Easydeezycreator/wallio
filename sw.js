850ec859e8bf776bde3babea29795d39fe62e077
Easydeezycreator <focuswaldo@gmail.com>
2026-08-17 22:34:50 -0700

    Catalogo de cartas de test con autorrelleno y parte de camara al final

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v2";
+const VERSION = "wallio-v3";
 const BASICOS = [
   "./",
   "./index.html",
