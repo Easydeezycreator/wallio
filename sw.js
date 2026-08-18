2e0a4de5e56d693136eff16b0c08b19aabfdd953
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 00:18:19 -0700

    El buzon pasa a su propia pestana, la ultima, y hidden vuelve a ocultar

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v7";
+const VERSION = "wallio-v8";
 const BASICOS = [
   "./",
   "./index.html",
