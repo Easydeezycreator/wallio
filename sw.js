a6e53cd4659e490b39829dc5cee969615fdc3601
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 20:51:03 -0700

    v37: fuera el correo del estado

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v36";
+const VERSION = "wallio-v37";
 const BASICOS = [
   "./",
   "./index.html",
