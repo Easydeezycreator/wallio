ffca4db3de8a215fd3d30186c8650249548ab197
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 20:24:53 -0700

    parte v23: mismos arreglos de edicion, memoria de camaras y autofill en la app del parte

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v22";
+const VERSION = "wallio-parte-v23";
 const BASICOS = [
   "./",
   "./index.html",
