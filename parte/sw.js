2d7e5b66612fb7b2fb4dd986a71a8acfce6fa25d
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 18:55:18 -0700

    parte v20: mismas mejoras de la v30 en la app del parte

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v19";
+const VERSION = "wallio-parte-v20";
 const BASICOS = [
   "./",
   "./index.html",
