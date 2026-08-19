3ce6a2113c8e53a36e812307e22173d5220085b7
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 22:20:02 -0700

    parte v33: rediseno moderno del parte

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v32";
+const VERSION = "wallio-parte-v33";
 const BASICOS = [
   "./",
   "./index.html",
