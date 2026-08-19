b7874fc6bb8dd09e8e2e3fc8a0456803e6ef3338
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 20:24:26 -0700

    v35: la camara (y foquista/DoP) siempre editables, + otra camara con memoria por modo, y autofill de Chrome capado en todo el parte

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v34";
+const VERSION = "wallio-v35";
 const BASICOS = [
   "./",
   "./index.html",
