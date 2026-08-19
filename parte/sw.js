d61cea1f4088c94082924b3079e4aaa0220a749b
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 21:50:51 -0700

    parte v31: version visible en la Guia

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v30";
+const VERSION = "wallio-parte-v31";
 const BASICOS = [
   "./",
   "./index.html",
