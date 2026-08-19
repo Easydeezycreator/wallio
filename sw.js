eeca46b89049f822a2e4a59bd418b5a1ec1de928
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 22:19:26 -0700

    v45: rediseno moderno del parte

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v44";
+const VERSION = "wallio-v45";
 const BASICOS = [
   "./",
   "./index.html",
