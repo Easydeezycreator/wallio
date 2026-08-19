c0e69db575c32b9b72a13a81a7ffb855bcaf0c00
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 21:15:14 -0700

    v39: en film sin DIT, con loader

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v38";
+const VERSION = "wallio-v39";
 const BASICOS = [
   "./",
   "./index.html",
