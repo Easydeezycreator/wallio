ab2fa57a758b796a0c8b30ee11fbdf96721d0664
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 22:05:53 -0700

    parte v32: rediseno del parte impreso

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v31";
+const VERSION = "wallio-parte-v32";
 const BASICOS = [
   "./",
   "./index.html",
