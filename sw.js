91c6313f927e7544e54b439c3c1598280383287a
Easydeezycreator <focuswaldo@gmail.com>
2026-08-18 19:22:13 -0700

    v31: avisos de cuenta siempre visibles + validacion clara del registro

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v30";
+const VERSION = "wallio-v31";
 const BASICOS = [
   "./",
   "./index.html",
