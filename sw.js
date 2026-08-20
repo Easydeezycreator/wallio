3960a25165b2eb01e1f1151e604a74bbe8b376dc
Easydeezycreator <focuswaldo@gmail.com>
2026-08-20 18:37:23 +0200

    v48: multicamara A-D con colores

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v47";
+const VERSION = "wallio-v48";
 const BASICOS = [
   "./",
   "./index.html",
