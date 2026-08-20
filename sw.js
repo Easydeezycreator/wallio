68575aa39f895446afe423a358ad0d8cbac2f0f1
Easydeezycreator <focuswaldo@gmail.com>
2026-08-20 17:58:54 +0200

    v47: film Cargado por + foco en escena nueva

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    próxima vez: así nunca esperas a la red, pero tampoco te quedas anclado
    a una versión vieja.                                                     */
 
-const VERSION = "wallio-v46";
+const VERSION = "wallio-v47";
 const BASICOS = [
   "./",
   "./index.html",
