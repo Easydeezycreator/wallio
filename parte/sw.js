7405292f1a6c202f2af8066994eef3cf159c0e3f
Easydeezycreator <focuswaldo@gmail.com>
2026-08-20 19:10:09 +0200

    parte v37: parte compartido entre auxiliares

--- parte/sw.js
+++ parte/sw.js
@@ -2,7 +2,7 @@
    Caché aparte de la plataforma entera: son dos apps distintas en el
    teléfono y no se pisan la una a la otra.                              */
 
-const VERSION = "wallio-parte-v36";
+const VERSION = "wallio-parte-v37";
 const BASICOS = [
   "./",
   "./index.html",
