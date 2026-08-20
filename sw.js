d3b3e2f8016bbcde27bece273d5292400741070e
Easydeezycreator <focuswaldo@gmail.com>
2026-08-20 23:38:28 +0200

    v66: el proyecto se comparte solo

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    (eso daba ERR_FAILED con el sitio perfectamente vivo), y aplana las
    respuestas redirigidas, que el navegador rechaza en navegaciones. */
 
-const VERSION = "wallio-v65";
+const VERSION = "wallio-v66";
 const FAMILIA = "wallio-v";
 const AJENA = "";
 const BASICOS = [
